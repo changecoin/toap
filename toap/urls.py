0537eb82f4b7af2c102fb3fbec79032549123add
Nick Sullivan <nick@sullivanflock.com>
2015-04-17 12:31:00 -0700

    plane view

--- toap/urls.py
+++ toap/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 
 urlpatterns = [
     # Examples:
+    url(r'^plane', 'toap.views.plane', name='plane'),
     url(r'^$', 'toap.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 ]
--- toap/views.py
+++ toap/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render_to_response
 def home(request):
     context = {}
     return render_to_response('home.html', context, context_instance=RequestContext(request))
+
+
+def plane(request):
+    context = {"tip_url": request.GET.get("tip_url")}
+    return render_to_response('plane.html', context, context_instance=RequestContext(request))

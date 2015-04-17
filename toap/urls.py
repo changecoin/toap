83a417a77fe7b4919ce6b58267815c91efab2151
Nick Sullivan <nick@sullivanflock.com>
2015-04-17 13:11:29 -0700

    hello static

--- toap/urls.py
+++ toap/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # Examples:
     url(r'^plane', 'toap.views.plane', name='plane'),
     url(r'^$', 'toap.views.home', name='home'),
-]
+] + static("/public", document_root=settings.BASE_DIR + "/public")

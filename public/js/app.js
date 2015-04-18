57bb4435f786b80ea520f0ebdd336c2bc5719fe9
Christian Williams <orbit@framezero.com>
2015-04-17 19:26:52 -0700

    all the plumbing works

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-/* hello world */
+$(function() {
+    $(".tip-form [type='text']").on("keypress", function(e) {
+        if (e.keyCode === "13") {
+            $(".tip-form").submit();
+        }
+    });
+
+    $(".tip-form").on("submit", function(e) {
+        e.preventDefault();
+
+        $.post("/tip-url", $(".tip-form").serialize(), function(data) {
+            $("body").addClass("done");
+            $(".qr")
+                .attr("src", "https://api.qrserver.com/v1/create-qr-code/?data=" + data.magic_url + "&size=320x320");
+            $(".link .text span").text(data.magic_url);
+        });
+    });
+
+    $(".print a").on("click", function(e) {
+        e.preventDefault();
+        window.print();
+    });
+});
